d26f8629347c013b7741a05348111dc56492b652
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2016-02-16 12:15:56 +0100

    Added missing import

--- src/main/java/io/warp10/sensision/Sensision.java
+++ src/main/java/io/warp10/sensision/Sensision.java
@@ -20,6 +20,7 @@ import io.warp10.sensision.FilePoller;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
