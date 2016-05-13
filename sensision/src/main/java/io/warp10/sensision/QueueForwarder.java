fa201f1cef559a64eeb558a9cc40ad1d4a744115
David Morin <morin.david.bzh@gmail.com>
2016-05-13 11:46:51 +0200

    delete auto import

--- sensision/src/main/java/io/warp10/sensision/QueueForwarder.java
+++ sensision/src/main/java/io/warp10/sensision/QueueForwarder.java
@@ -18,7 +18,15 @@ package io.warp10.sensision;
 
 import io.warp10.sensision.Sensision.Value;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
