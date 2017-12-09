16816daa34b12e45cfa59e4bf113372c5dd0741d
David Morin <morin.david.bzh@gmail.com>
2017-12-09 01:28:54 +0100

    Some fixes in groovy scripts

--- sensision/src/main/groovy/linux.proc.net.sockstat.groovy
+++ sensision/src/main/groovy/linux.proc.net.sockstat.groovy
@@ -24,6 +24,10 @@
 import java.io.PrintWriter;
 import static io.warp10.sensision.Utils.*;
 
+BufferedReader br = null;
+PrintWriter pw = null;
+File tmpfile = null;
+
 try {
 
   populateSymbolTable(this);
@@ -55,11 +59,9 @@ try {
   //
 
   File outfile = OUTFILE;
-  File tmpfile = new File("${OUTFILE.getAbsolutePath()}.pending");
+  tmpfile = new File("${OUTFILE.getAbsolutePath()}.pending");
 
-  PrintWriter pw = new PrintWriter(tmpfile);
-
-  BufferedReader br = null;
+  pw = new PrintWriter(tmpfile);
 
   labels = [:];
   labels.putAll(commonLabels);
@@ -115,5 +117,12 @@ try {
   if (SHOW_ERRORS) { e.printStackTrace(System.err); }
 } finally {
   try { if (null != br) br.close(); } catch (IOException ioe) {}
-  try { if (null != pw) pw.close(); } catch (IOException ioe) {}
+  try {
+    if (null != pw) {
+      pw.close();
+      if ((tmpfile.exists()) && (0 == tmpfile.length())) {
+        tmpfile.delete()
+      }
+    }
+  } catch (IOException ioe) {}
 }
